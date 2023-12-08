564d19c3efc1f2b2d4c95918dfc0ad4ade79b7d3
Vaibhav Singh <79097275+vaibhavsingh31390@users.noreply.github.com>
2023-12-08 14:40:28 +0530

    Update FormOverlay.jsx Transition added

--- src/Components/Form/FormOverlay.jsx
+++ src/Components/Form/FormOverlay.jsx
@@ -1,12 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import contactContext from "../../store/contact-context";
 
 const FormOverlay = () => {
+  const [isActive, setIsActive] = useState(false);
   const ctx = useContext(contactContext);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsActive(true);
+    }, 50);
+    return () => clearTimeout(timer);
+  }, [ctx]);
   return (
     <div
-      className="contact-overlay"
+      className={`contact-overlay ${isActive ? "active" : ""}`}
       onClick={() => {
         ctx.setContactVisible(false);
       }}
